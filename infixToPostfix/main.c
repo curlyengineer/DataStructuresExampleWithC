dfe873783966308ef0498f28700df26753eddddd
curlyengineer <58690262+curlyengineer@users.noreply.github.com>
2021-01-17 08:05:36 +0300

    example

---
+++ infixToPostfix/main.c
@@ -0,0 +1,102 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct Stack{
+	//top indisi -1 iken yýðýn boþtur 
+	int top ;
+	//Kapasite belirleme
+	unsigned capacity;
+	int* array;
+};
+//Yýðýn yaratma fonksiyonu
+struct Stack* createStack(unsigned capacity){
+	struct Stack* stack=(struct Stack*)malloc(sizeof(struct Stack));
+	if(!stack)
+	return NULL;
+	stack->top=-1;
+	stack->capacity=capacity;
+	stack->array=(int*)malloc (stack->capacity*sizeof(int));
+	 if(!stack->array)
+	 return NULL;
+	return stack;
+}
+//Dizi baþ kontrolü
+int isEmpty(struct Stack* stack){
+	return stack->top==-1;
+}
+//Yýðýn top deðer gösterme
+char peek(struct Stack* stack){
+	return stack->array[stack->top];
+}
+//yýðýndan eleman alma fonksiyonu
+char pop (struct Stack* stack){
+	if(!isEmpty(stack))
+	return stack->array[stack->top--];
+	return '$';
+}
+//Yýðýna eleman ekleme fonksiyonu
+void push(struct Stack* stack, char op){
+	stack->array[++stack->top]=op;
+}
+
+//Eleman operand mý?
+int isOperand(char ch){
+	return (ch>='a'&&ch<='z'||ch>='A'&&ch<='Z');
+}
+//+ - öncelik kontrolü
+int Prec(char ch){
+	switch(ch){
+		case '+':
+		case '-':
+		return 1;
+		case '*':
+		case '/':
+		return 2;
+		case '^':
+		return 3;
+	}
+	return -1;
+}
+//Ana fonksiyon
+int infixToPostfix(char* exp){
+	int i,k;
+	struct Stack* stack=createStack(strlen(exp));
+	if(!stack)
+	return -1;
+	for(i=0,k=-1;exp[i];++i)
+	{
+	if(isOperand(exp[i]))
+	exp[++k]=exp[i];
+	else
+	if(exp[i]=='(')
+	push(stack,exp[i]);
+	else
+	if(exp[i]==')')
+	{
+		while(!isEmpty(stack)&&peek(stack)!='(')
+		exp[++k]=pop(stack);
+		if(!isEmpty(stack)&&peek(stack)!='(')
+		return -1;
+		else 
+		pop(stack);
+	}
+	else
+	{
+		while(!isEmpty(stack)&&Prec(exp[i])<=Prec(peek(stack)))
+		exp[++k]=pop(stack);
+		push(stack,exp[i]);
+	}
+}
+while (!isEmpty(stack))
+exp[++k]=pop(stack);
+exp[++k]='\0';
+printf("%s\n",exp);
+}
+
+int main(int argc, char *argv[]) {
+	char exp[]="a+b*(c^d-e)^(f+g*h)-i";
+	infixToPostfix(exp);
+	return 0;
+}
---
+++ tree/main.c
@@ -0,0 +1,82 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+struct tree{
+	int data;
+	struct tree *left;
+	struct tree *right;
+};
+struct tree *createNode(int item){
+	struct tree *yeni=(struct tree*)malloc (sizeof(struct tree));
+	yeni->data=item;
+	yeni->left=yeni->right=NULL;
+	return yeni;
+}
+void inOrder(struct tree *root){
+	if(root==NULL)
+	return;
+	inOrder(root->left);
+	printf("%d\t",root->data);
+	inOrder(root->right);
+}
+void postOrder(struct tree *root){
+	if(root==NULL) return ;
+	postOrder(root->left);
+	postOrder(root->right);
+	printf("%d \t", root->data);
+}
+void preOrder(struct tree *root){
+	if(root==NULL) return ;
+	printf("%d \t", root->data);
+	preOrder(root->left);
+	preOrder(root->right);
+}
+struct tree *insert(struct tree *root, int item){
+	if(root==NULL)return createNode(item);
+	if(item<root->data)
+	root->left=insert(root->left,item);
+	else if(item>root->data)
+	root->right=insert(root->right,item);
+	return root ;
+}
+// for  Binary Search Tree
+int theBiggestBinary(struct tree *root){
+	if(root==NULL) return 0;
+	while(root->right!=NULL){
+		root=root->right;
+	}
+	int sonuc=root->data;
+	return sonuc;
+} 
+// for other tree
+int theBiggest(struct tree *root){
+	if(root==NULL)return 0;
+	int max=root->data;
+	int lVal=theBiggest(root->left);
+	int rVal=theBiggest(root->right);
+	if(lVal>rVal)
+	max=lVal;
+	if(rVal>lVal)
+	max=rVal;
+	return max;
+}
+int main(int argc, char *argv[]) {
+	struct tree *root=NULL;
+	root=createNode(5);
+	root->left=createNode(3);
+	root->left->left=createNode(1);
+	root->left->right=createNode(4);
+	root->right=createNode(8);
+	root->right->right=createNode(10);
+	root->right->left=createNode(7);
+	insert(root,6);
+	inOrder(root);
+	printf("\n");
+	preOrder(root);
+	printf("\n");
+	postOrder(root);
+	printf("\n");
+	printf("En buyuk Yaprak %d",theBiggestBinary(root));
+	return 0;
+}
